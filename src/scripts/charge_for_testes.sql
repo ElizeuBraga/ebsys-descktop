f8c1d34c5132bba0fb60a70d008027877fb62166
Elizeu <elizeubragasantos@gmail.com>
2021-02-13 03:42:21 -0300

    new database

---
+++ src/scripts/charge_for_testes.sql
@@ -0,0 +1,72 @@
+-- Sections
+INSERT INTO sections(name)VALUES('Hamburguers');
+INSERT INTO sections(name)VALUES('Taxas');
+-- products
+INSERT INTO products(name, price, section_id, ask_obs)VALUES('Bomba atômica', 7.99, (select max(id) from sections where name = 'Hamburguers' limit 1), TRUE);
+INSERT INTO products(name, price, section_id, ask_obs)VALUES('X tudo', 11.99, (select max(id) from sections where name = 'Hamburguers' limit 1), TRUE);
+INSERT INTO products(name, price, section_id, ask_obs)VALUES('Taxa de entrega 1', 3.00, (select max(id) from sections where name = 'Taxas' limit 1), TRUE);
+INSERT INTO products(name, price, section_id, ask_obs)VALUES('Taxa de entrega 2', 5.00, (select max(id) from sections where name = 'Taxas' limit 1), TRUE);
+
+-- profiles
+insert into profiles(name)values('Adimin');
+
+-- users
+insert into users(name,email,phone,password,token,updated,change_password,profile_id)values('Elizeu', 'elizeu@gmail.com', '61998636231', '$2y$10$sXZ2Yw.SdaUwJIoLpx9OKuvT.Y1c0uGisD4tcHBitL/n6SospzRXy',
+	'12345', true, true, (select id from profiles where name = 'Adimin' limit 1)
+);
+
+-- cities
+insert into cities (name,product_id)values('Sobradinho I', (select id from products where name = 'Taxa de entrega 1' limit 1));
+insert into cities (name,product_id)values('Sobradinho II', (select id from products where name = 'Taxa de entrega 2' limit 1));
+
+
+-- adresses
+insert into adresses (address,complemnt,city_id)values('Q15 Conj c cs 12', null, (select id from cities where name = 'Sobradinho I' limit 1));
+
+-- customers
+insert into customers (name,phone,address_id)values('Luana', '61998636232', (select id from adresses where address = 'Q15 Conj c cs 12' LIMIT 1));
+
+-- cashiers
+insert into cashiers (user_id)values((select id from users where name = 'Elizeu' LIMIT 1));
+
+-- payments
+INSERT into payments(name)values('Money');
+-- payments cashiers
+
+INSERT into payments_cashiers(payment_id,cashier_id)values(
+	(select id from payments WHERE name = 'Money'),
+	(
+		SELECT c.id from cashiers c
+		join users u on u.id = c.user_id where u.name = 'Elizeu' limit 1
+	)
+);
+
+-- order_types
+insert into order_types(name)values('Delivery');
+insert into order_types(name)values('Balcão');
+
+-- orders
+INSERT orders(cashier_id,customer_id,order_types_id)values(
+	(
+		SELECT c.id from cashiers c
+		join users u on u.id = c.user_id where u.name = 'Elizeu' limit 1
+	),
+	(
+		SELECT id from customers where name = 'Luana' limit 1
+	),
+	(
+		SELECT id from order_types where name = 'Delivery' limit 1
+	)
+);
+
+-- items
+insert into items (quantity,product_id,price,order_id)values(
+	3,
+	(select id from products where name = 'Bomba atômica' limit 1),
+	(select price from products where name = 'Bomba atômica' limit 1),
+	(
+		SELECT c.id from customers c
+		join orders o on o.customer_id = c.id
+		where c.name = 'Luana' limit 1
+	)
+);
---
+++ src/scripts/database_mysql.sql
@@ -0,0 +1,181 @@
+begin;
+	drop database if exists ebsys_descktop;
+	create database if not exists ebsys_descktop;
+	use ebsys_descktop;
+	-- made in ws
+	CREATE table if not exists pages(
+		id integer auto_increment primary key,
+		name varchar(50) not null,
+		model varchar(50),
+		url varchar(100),
+		visible boolean default false,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		updated_at TIMESTAMP null,
+		deleted_at TIMESTAMP null
+	);
+	
+	-- made in ws
+	CREATE table if not exists sections(
+		id integer auto_increment primary key,
+		name varchar(50) not null,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		updated_at TIMESTAMP null,
+		deleted_at TIMESTAMP null
+	);
+	
+	-- made in ws
+	create table if not exists products (
+		id integer auto_increment primary key,
+		name varchar(50) not null,
+		price real not null,
+		section_id integer not null,
+		ask_obs boolean not null default 1,
+		updated boolean not null default 1,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		updated_at TIMESTAMP null,
+		deleted_at TIMESTAMP null,
+		foreign key (section_id) references sections(id)
+	);
+	
+	CREATE table profiles(
+		id integer auto_increment primary key,
+		name varchar(15),
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		updated_at timestamp null,
+		deleted_at timestamp null
+	);
+	-- made in ws
+	CREATE table if not exists users(
+		id integer auto_increment primary key,
+		name varchar(20) not null,
+		email varchar(50) unique not null,
+		phone varchar(11) unique not null,
+		password varchar(60) not null,
+		token varchar(60),
+		updated boolean not null default 1,
+		change_password boolean not null default 1,
+		profile_id integer not null,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		updated_at timestamp null,
+		deleted_at timestamp null,
+		foreign key (profile_id) references profiles(id)
+	);
+	
+	-- made in ws
+	CREATE table if not exists cities(
+		id integer auto_increment primary key,
+		name varchar(20) not null,
+		updated boolean not null default 1,
+		product_id integer not null,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		updated_at timestamp null,
+		deleted_at timestamp null,
+		foreign key (product_id) references products (id)
+	);
+	
+	-- made in ws
+	CREATE table if not exists adresses(
+		id integer auto_increment primary key,
+		address varchar(30) not null,
+		complemnt varchar(20),
+		updated boolean not null default 1,
+		city_id integer not null,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		updated_at timestamp null,
+		deleted_at timestamp null,
+		foreign key (city_id) references cities (id)
+		
+	);
+	-- made in local
+	CREATE table if not exists customers(
+		id integer auto_increment primary key,
+		name varchar(50) not null,
+		phone varchar(11) unique not null,
+		updated boolean not null default 1,
+		address_id integer not null,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		updated_at timestamp null,
+		deleted_at timestamp null,
+		foreign key (address_id) references adresses(id)
+	);
+	
+	-- made in local
+	create table if not exists cashiers(
+		id integer auto_increment primary key,
+		user_id integer not null,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		updated_at timestamp null,
+		deleted_at timestamp null,
+		foreign key (user_id) references users (id)
+	);
+	
+	-- made in local
+	create table if not exists payments_cashiers(
+		id integer auto_increment primary key,
+		payment_id integer not null,
+		cashier_id integer not null,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		updated_at timestamp null,
+		deleted_at timestamp null,
+		foreign key (payment_id) references payments (id),
+		foreign key (cashier_id) references cashiers (id)
+	);
+	
+	
+	-- made in local
+	CREATE table if not exists order_types(
+		id integer auto_increment primary key,
+		name varchar(10),
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		updated_at timestamp null,
+		deleted_at timestamp null
+	);
+
+	-- made in local
+	CREATE table if not exists orders(
+		id integer auto_increment primary key,
+		cashier_id integer not null,
+		customer_id integer,
+		order_types_id integer not null,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		updated_at timestamp null,
+		deleted_at timestamp null,
+		foreign key (cashier_id) references cashiers (id),
+		foreign key (customer_id) references customers (id),
+		foreign key (order_types_id) references order_types (id)
+	);
+	
+	-- made in local
+	create table if not exists items(
+		id integer auto_increment primary key,
+		quantity integer not null,
+		product_id integer not null,
+		price real not null,
+		order_id integer not null,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		updated_at timestamp null,
+		deleted_at timestamp null,
+		foreign key (order_id) references orders (id),
+		foreign key (product_id) references products (id)
+	);
+
+	create table if not exists payments(
+		id integer auto_increment primary key,
+		name varchar(7),
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		updated_at timestamp null,
+		deleted_at timestamp null
+	);
+
+	create table if not exists payments_orders(
+		id integer auto_increment primary key,
+		order_id integer not null,
+		payment_id integer not null,
+		price real not null,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		updated_at timestamp null,
+		deleted_at timestamp null,
+		foreign key (payment_id) references products (id),
+		foreign key (order_id) references products (id)
+	);
+commit;
