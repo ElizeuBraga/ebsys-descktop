b713d3cf3259176ab67ad7dab3b94caaee4656fa
Elizeu <elizeubragasantos@gmail.com>
2021-03-18 23:36:46 -0300

    cleaning mixins code

--- src/mixins/mixins.js
+++ src/mixins/mixins.js
@@ -1,10 +1,5 @@
-import axios from 'axios';
-import { Helper } from "../models/Helper";
-import {Payment} from '../models/Payment';
 import EventBus from '../EventBus';
-import Swal from "sweetalert2";
-
-let helper = new Helper();
+import {Payment} from '../models/Payment';
 export default {
     data() {
         return {
@@ -17,29 +12,9 @@ export default {
 
     async created() {
         EventBus.$on("change-tab", (e) => {
-            // this.reset();
             this.tabIndex = e;
-            // if (e == 1) {
-            //   this.initDeliveryOrder();
-            // }
         });
         this.paymentsFormats = await new Payment().all();
-        // if(localStorage.employees){
-        //     this.employees = localStorage.employees
-        // }
-
-        // axios.defaults.baseURL = 'http://192.168.1.87:8080/ebsys/public_html/api/'
-        // // axios.defaults.baseURL = 'https://api-api-api-api.herokuapp.com/api/'
-        // // axios.defaults.headers.common["Host"] = "http://localhost:8000/api/";
-        // axios.defaults.headers.common["Content-Type"] = "application/json";
-        // axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
-        // axios.defaults.headers.common["Access-Control-Allow-Headers"] =
-        //     "Content-Type";
-        // axios.defaults.headers.common["Authorization"] =
-        //     "Bearer j2MFvmcsZ5RQY6Wn33q5VdRcafIm8lb2iko8zEeVvkyBNwl67gSdpjI31F9f";
-        // // console.log(this.products)
-        // //$2a$10$YsoadGu/kTz9nHSqHINHAukOVgYT.ViRvKxc90SgPqffl1kWcIAve
-        // // $2y$10$Sb52MpW2MnpAQreuDbuHk.IZozue2BME74sGp7Xq9E5mBtS/4LvY2
     },
 
     computed: {
